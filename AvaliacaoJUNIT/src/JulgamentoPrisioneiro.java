f8c41025bf9f15fd04cbebedd1d7be1327c77e37
Renan Augusto Alves da Costa <30834451+renan-alves@users.noreply.github.com>
2020-04-16 20:12:47 -0300

    Primeira parte da documentação (Criação do JUNIT)

---
+++ AvaliacaoJUNIT/src/JulgamentoPrisioneiro.java
@@ -0,0 +1,10 @@
+
+public class JulgamentoPrisioneiro {
+
+	public int calculaPena(String respostaPrisioneiroA, String respostaPrisioneiroB) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
