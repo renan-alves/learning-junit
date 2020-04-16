4535f5e4b8edf3783bf5e92cef89785d27ee3f8e
renan-alves <renan.a.a.costa@gmail.com>
2020-04-16 20:46:03 -0300

    Atualização da documentação (Primeira correção)

--- AvaliacaoJUNIT/src/JulgamentoPrisioneiro.java
+++ AvaliacaoJUNIT/src/JulgamentoPrisioneiro.java
@@ -6,15 +6,15 @@ public class JulgamentoPrisioneiro {
 	   private int PENA_CONDENACAO_INDIVIDUAL = 10;
 	   private int PENA_CONDENACAO_CUMPLICES = 11;
 
-	   public int calculaPena(Resposta respostaPrisioneiroA, Resposta respostaPrisioneiroB) {
-	      if (respostaPrisioneiroA == Resposta.DELACAO) {
-	          if (respostaPrisioneiroB == Resposta.DELACAO) {
+	   public int calculaPena(String respostaPrisioneiroA, String respostaPrisioneiroB) {
+	      if (respostaPrisioneiroA == "Culpado") {
+	          if (respostaPrisioneiroB == "Culpado") {
 	            return PENA_CONDENACAO_MUTUA;
 	          } else {
 	                 return PENA_INOCENCIA;
 	                 }
 	       } else {
-	                if (respostaPrisioneiroB == Resposta.DELACAO) {
+	                if (respostaPrisioneiroB == "Culpado") {
 	                    return PENA_CONDENACAO_INDIVIDUAL;
 	                } else {
 	                         return PENA_CONDENACAO_CUMPLICES;
