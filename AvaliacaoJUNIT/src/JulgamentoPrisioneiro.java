71acdb3cee790007c56d46940c12a699c237de94
renan-alves <renan.a.a.costa@gmail.com>
2020-04-16 21:06:30 -0300

    Alteração da documenação (Ultima correção)

--- AvaliacaoJUNIT/src/JulgamentoPrisioneiro.java
+++ AvaliacaoJUNIT/src/JulgamentoPrisioneiro.java
@@ -2,24 +2,24 @@
 public class JulgamentoPrisioneiro {
 
 	private int PENA_INOCENCIA = 0;
-	   private int PENA_CONDENACAO_MUTUA = 5;
-	   private int PENA_CONDENACAO_INDIVIDUAL = 10;
-	   private int PENA_CONDENACAO_CUMPLICES = 1;
+	private int PENA_CONDENACAO_MUTUA = 5;
+	private int PENA_CONDENACAO_INDIVIDUAL = 10;
+	private int PENA_CONDENACAO_CUMPLICES = 1;
 
-	   public int calculaPena(String respostaPrisioneiroA, String respostaPrisioneiroB) {
-	      if (respostaPrisioneiroA == "Culpado") {
-	          if (respostaPrisioneiroB == "Culpado") {
-	            return PENA_CONDENACAO_MUTUA;
-	          } else {
-	                 return PENA_INOCENCIA;
-	                 }
-	       } else {
-	                if (respostaPrisioneiroB == "Culpado") {
-	                    return PENA_CONDENACAO_INDIVIDUAL;
-	                } else {
-	                         return PENA_CONDENACAO_CUMPLICES;
-	                        }
-	                }
-	     }
-	
+	public int calculaPena(String respostaPrisioneiroA, String respostaPrisioneiroB) {
+		if (respostaPrisioneiroA == "Culpado") {
+			if (respostaPrisioneiroB == "Culpado") {
+				return PENA_CONDENACAO_MUTUA;
+			} else {
+				return PENA_CONDENACAO_INDIVIDUAL;
+			}
+		} else {
+			if (respostaPrisioneiroB == "Culpado") {
+				return PENA_CONDENACAO_CUMPLICES;
+			} else {
+				return PENA_INOCENCIA;
+			}
+		}
+	}
+
 }
